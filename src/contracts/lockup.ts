ec344723eabafe956ddb2566be85090733ba5ede
David Case <david@fyxgaming.com>
2023-08-18 22:06:01 -0400

    Add nSequence check

--- src/contracts/lockup.ts
+++ src/contracts/lockup.ts
@@ -26,6 +26,7 @@ export class Lockup extends SmartContract {
     @method()
     public redeem(sig: Sig, pubkey: PubKey) {
         assert(this.ctx.locktime < 500000000, 'must use blockHeight locktime')
+        assert(this.ctx.sequence < 0xffffffff, 'must use sequence locktime')
         assert(
             this.ctx.locktime >= this.lockUntilHeight,
             'lockUntilHeight not reached'
